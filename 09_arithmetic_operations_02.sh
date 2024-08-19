7d90ec90b7ef0ed5dba5d3f060296897e0c6306b
Arafat Sarkar <98816004+Anon3605@users.noreply.github.com>
2024-08-19 21:13:01 +0600

    Update 09_arithmetic_operations_02.sh

--- 09_arithmetic_operations_02.sh
+++ 09_arithmetic_operations_02.sh
@@ -9,15 +9,15 @@ read -p "Do you wanna install (0/1): " ans
 
 if [ $ans == 1 ]
 then
-	sudo apt install bc
-	read -p "Do you wanna see the Manual?(0/1) " ans
-	if [ $ans == 1 ]
-	then
-		echo "Press q to quit"
-		man bc
-	else
-		echo "Never Mind"
-	fi
+        sudo apt install bc
+        read -p "Do you wanna see the Manual?(0/1) " ans
+        if [ $ans == 1 ]
+        then
+                echo "Press q to quit"
+                man bc
+        else
+                echo "Never Mind"
+        fi
 else
         read -p "Do you wanna see the Manual?(0/1) " ans
         if [ $ans == 1 ]
@@ -27,20 +27,15 @@ else
         else
                 echo "Never Mind"
         fi
-
 fi
 
 echo "Signs are Addition(+), Subtraction(-), Multiplication(x), 
-Division(/), exponential(xx), Modulas(%)"
+Division(/), exponential(xx), Modulus(%)"
 echo "For a quick reminder, In shell, * represents all files in the current directory.
 So, in order to use * as a multiplication operator, we should escape it like \*.
-If we directly use * in expr, we will get error message."
+If we directly use * in expr, we will get an error message."
 
 read -p "Give the sign here: " sign
-
-
-
-
 
 sum="${numbers[0]}+${numbers[1]}"
 sub="${numbers[0]}-${numbers[1]}"
@@ -52,31 +47,45 @@ sub_result=$(echo "$sub" | bc)
 mul_result=$(echo "$mul" | bc)
 pow_result=$(echo "$pow" | bc)
 
+if (( numbers[0] >= 0 )) && (( numbers[1] >= 0 )); then
+        sqrt_result01=$(echo "scale=2;sqrt(${numbers[0]})" | bc)
+        sqrt_result02=$(echo "scale=2;sqrt(${numbers[1]})" | bc)
+else
+        if (( numbers[0] >= 0 )); then
+                sqrt_result01=$(echo "scale=2;sqrt(${numbers[0]})" | bc)
+                sqrt_result02="Negative values cannot be rooted"
+        elif (( numbers[1] >= 0 )); then
+                sqrt_result02=$(echo "scale=2;sqrt(${numbers[1]})" | bc)
+                sqrt_result01="Negative values cannot be rooted"
+        else
+                sqrt_result01="Negative values cannot be rooted"
+                sqrt_result02="Negative values cannot be rooted"
+        fi
+fi
 
 if (( numbers[1]!=0 ))
 then
         div="${numbers[0]}/${numbers[1]}"
         div_result=$(echo "$div" | bc)
-	mod="${numbers[0]}%${numbers[1]}"
+        mod="${numbers[0]}%${numbers[1]}"
         mod_result=$(echo "$mod" | bc)
-
 else
-        div_result="Not possible to calculate"
-        mod_result="Not possible to calculate"
+        div_result="Not possible to calculate, 2nd value is 0"
+        mod_result="Not possible to calculate 2nd value is 0"
 fi
 
 if [ $sign == "+" ]
 then
         echo $sum | bc
-	sum_result=$(echo "$sum" | bc)
+        sum_result=$(echo "$sum" | bc)
 elif [ $sign == "-" ]
 then
         echo $sub | bc
-	sub_result=$(echo "$sub" | bc)
+        sub_result=$(echo "$sub" | bc)
 elif [ $sign == "x" ]
 then
         echo $mul | bc
-	mul_result=$(echo "$mul" | bc)
+        mul_result=$(echo "$mul" | bc)
 elif [ $sign == "/" ]
 then
         echo $div_result
@@ -86,10 +95,13 @@ then
 elif [ $sign == "xx" ]
 then
         echo $pow | bc
+elif [ $sign == "root" ]
+then
+        echo "The square root of value 01 is $sqrt_result01"
+        echo "The square root of value 02 is $sqrt_result02"
 else
         echo "Wrong Sign Given"
 fi
-
 
 sleep 3
 echo "echo "expression" | bc"
@@ -101,12 +113,15 @@ echo "To store in a variable we use this
 result=$ (echo "$ value" | bc)
 Ans the results are stored"
 echo "The Summation of two numbers is $sum_result"
-echo "The Subtration of two numbers is $sub_result"
+echo "The Subtraction of two numbers is $sub_result"
 echo "The Multiplication of two numbers is $mul_result"
-echo "The Divisiion of two numbers is $div_result"
+echo "The Division of two numbers is $div_result"
 echo "The Power of first over second is $pow_result"
 echo "The Modulus of two numbers is $mod_result"
-echo "You just need to give the dollar sign before ecery variable while using,
+echo "Square Root of two numbers are: 
+        1st integer: $sqrt_result01
+        2nd integer: $sqrt_result02"
+echo "You just need to give the dollar sign before every variable while using,
 result=$ (echo "$ value" | bc)"
 echo ""
 echo "Thank You"
